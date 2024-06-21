64a4fb1882fa1bb2ec8ab9476adbb5e1cb06d263
StefanAngelovski <stefanangelovski2002@gmail.com>
2024-06-21 17:00:43 +0200

    Truncated unneccessary elements for deployment
    Ready for deployment

--- mainApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'mainApp'
-
-urlpatterns = [
-    path("", views.display_index, name='index'),
-    path("about/", views.display_about, name='about'),
-]
